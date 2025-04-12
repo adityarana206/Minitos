f1472812dc3973bf065f5b9984ffdae2e92a7764
Aditya <adityarana206@gmail.com>
2025-04-12 18:16:21 +0400

    Code review

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import AppNavigation from './src/navigation/Index';
-import CategoryWise from './src/screen/Productfilter';
+import CategoryWise from './src/screen/Product.screen';
 
 
 function App(): React.JSX.Element {
